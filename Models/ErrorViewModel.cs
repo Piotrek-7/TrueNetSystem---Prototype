8c75a5dc24655edb9b56ae9b6d4cdd190b73ecb7
unknown <piotrek.01office@gmail.com>
2025-05-12 19:16:11 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrueNetSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
